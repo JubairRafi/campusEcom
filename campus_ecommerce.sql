b67d4fcf8af2d99aa0b11a1fb64bd72bab72e329
JubairRafi <jubairpial96960@gmail.com>
2020-12-25 03:18:34 +0600

    db updated

--- campus_ecommerce.sql
+++ campus_ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 23, 2020 at 07:32 PM
+-- Generation Time: Dec 24, 2020 at 09:58 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -34,6 +34,13 @@ CREATE TABLE `administrators` (
   `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `administrators`
+--
+
+INSERT INTO `administrators` (`teacherId`, `name`, `sex`, `password`) VALUES
+(17339401, 'ashik', 'male', 'admin');
+
 -- --------------------------------------------------------
 
 --
@@ -43,8 +50,31 @@ CREATE TABLE `administrators` (
 CREATE TABLE `commodities` (
   `commoditiesId` int(10) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `price` varchar(10) NOT NULL,
-  `picture` varchar(50) NOT NULL
+  `price` int(20) NOT NULL,
+  `picture` varchar(50) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `studentId` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `commodities`
+--
+
+INSERT INTO `commodities` (`commoditiesId`, `name`, `price`, `picture`, `quantity`, `studentId`) VALUES
+(1, 'Body spray', 50, '', 2, 1),
+(2, 'Guitar', 2000, '', 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderlist`
+--
+
+CREATE TABLE `orderlist` (
+  `orderid` int(5) NOT NULL,
+  `commodityName` varchar(30) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `price` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -55,10 +85,19 @@ CREATE TABLE `commodities` (
 
 CREATE TABLE `transaction` (
   `transactionid` int(10) NOT NULL,
-  `commodiesId` varchar(10) NOT NULL,
+  `commodiesName` varchar(10) NOT NULL,
   `studentId` int(10) NOT NULL,
-  `price` int(10) NOT NULL
+  `price` int(10) NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`transactionid`, `commodiesName`, `studentId`, `price`, `date`) VALUES
+(1, 'Guitar', 1, 2000, '2020-12-08'),
+(2, 'Body spray', 2, 50, '2020-12-09');
 
 -- --------------------------------------------------------
 
@@ -73,15 +112,17 @@ CREATE TABLE `user_table` (
   `collage` varchar(50) NOT NULL,
   `major` varchar(9) NOT NULL,
   `dormitory` varchar(10) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(30) NOT NULL,
+  `mobile` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user_table`
 --
 
-INSERT INTO `user_table` (`studentId`, `name`, `sex`, `collage`, `major`, `dormitory`, `password`) VALUES
-(1, 'jubair', 'male', 'ABC', 'CSE', 'Yes', '12345');
+INSERT INTO `user_table` (`studentId`, `name`, `sex`, `collage`, `major`, `dormitory`, `password`, `mobile`) VALUES
+(1, 'jubair', 'male', 'ABC', 'CSE', 'Yes', '12345', ''),
+(2, 'pial', 'male', 'dfr', 'unselect', '5', '1234567', '');
 
 --
 -- Indexes for dumped tables
@@ -98,6 +139,12 @@ ALTER TABLE `administrators`
 --
 ALTER TABLE `commodities`
   ADD PRIMARY KEY (`commoditiesId`);
+
+--
+-- Indexes for table `orderlist`
+--
+ALTER TABLE `orderlist`
+  ADD PRIMARY KEY (`orderid`);
 
 --
 -- Indexes for table `transaction`
@@ -119,25 +166,31 @@ ALTER TABLE `user_table`
 -- AUTO_INCREMENT for table `administrators`
 --
 ALTER TABLE `administrators`
-  MODIFY `teacherId` int(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `teacherId` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17339402;
 
 --
 -- AUTO_INCREMENT for table `commodities`
 --
 ALTER TABLE `commodities`
-  MODIFY `commoditiesId` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `commoditiesId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orderlist`
+--
+ALTER TABLE `orderlist`
+  MODIFY `orderid` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `transactionid` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `transactionid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_table`
 --
 ALTER TABLE `user_table`
-  MODIFY `studentId` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `studentId` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
